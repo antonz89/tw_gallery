791220889bd59fdb3d3490cae7d815b18a29f071
antonz89 <anton.zakoverya@codeimmersives.com>
2023-08-28 22:40:02 -0500

    uploading

--- src/components/body/Body.jsx
+++ src/components/body/Body.jsx
@@ -1,9 +1,12 @@
-import { SimpleGrid,Box, Image, Text, Icon} from "@chakra-ui/react";
+import { SimpleGrid,Box, Image, Text} from "@chakra-ui/react";
+import { Icon } from '@chakra-ui/react'
+import { AiOutlineHeart,AiTwotoneHeart } from "react-icons/ai";
+
 
 
 const Body =()=>{
     return(
-        <div id="gallery-body">
+        <>
             <SimpleGrid
                 minChildWidth='330px'
                 paddingTop={{base: '5px',lg:'50px' }} 
@@ -11,7 +14,7 @@ const Body =()=>{
            
                 <Box align='center' margin='20px'>
                     
-                    <Image src='https://bit.ly/dan-abramov' alt='Dan Abramov' height={{base: '300px', md:'450',lg: '450px' }}/>
+                    <Image src='https://3.bp.blogspot.com/-MOEVQYZK1CA/UTK7svoxbHI/AAAAAAAABn8/e55cnn6l8U4/s1600/DSC_3281.JPG' alt='Dan Abramov' height={{base: '300px', md:'450',lg: '450px' }}/>
                    
                 </Box>
 
@@ -19,29 +22,97 @@ const Body =()=>{
                     height={{base: '170px', md:'500',lg: '500px' }}
                     borderLeftColor={{base: 'white',lg: 'black' }}
                     borderTopColor='white'
-                    // borderBottom='white'
                     borderLeftStyle='solid'
                     borderWidth='1px'
                     
-                    borderBottomColor={{base: 'black',lg: 'white' }}
+                    borderBottomColor={{base: 'white',lg: 'white' }}
                     align="center"
                     paddingTop={{base: '5px',lg:'175px' }}
                     
                  
                     >
-                    
-                        <Text>Beautiful Title</Text>
+                        
+                        <Text as='i'><b>"Beautiful Title"</b></Text><br/>
                         <br/>
                         <Text>Lorem ipsum dolor sit amet consectetur, adipisicin.</Text>
                         <br/>
                         <Text>$450</Text>
+                        <br/>
+                        <Icon as={AiOutlineHeart}/>
+
+
+                </Box>
+
+                {/* <Center>
+                <Icon as={PiWaveTriangleDuotone}/>
+                <Icon as={PiWaveTriangleDuotone}/>
+                <Icon as={PiWaveTriangleDuotone}/>
+                <Icon as={PiWaveTriangleDuotone}/>
+                <Icon as={PiWaveTriangleDuotone}/>
+                <Icon as={PiWaveTriangleDuotone}/>
+                <Icon as={PiWaveTriangleDuotone}/>
+                <Icon as={PiWaveTriangleDuotone}/>
+                <Icon as={PiWaveTriangleDuotone}/>
+                <Icon as={PiWaveTriangleDuotone}/>
+                <Icon as={PiWaveTriangleDuotone}/>
+                <Icon as={PiWaveTriangleDuotone}/>
+                <Icon as={PiWaveTriangleDuotone}/>
+                <Icon as={PiWaveTriangleDuotone}/>
+                <Icon as={PiWaveTriangleDuotone}/>
+                <Icon as={PiWaveTriangleDuotone}/>
+                </Center> */}
+
+            
+            </SimpleGrid>
+
+            {/* --------------------Second Image------------------- */}
+
+            <SimpleGrid
+                minChildWidth='330px'
+                paddingTop={{base: '40px',lg:'100px' }} 
+                >
+           
+                <Box align='center' margin='20px'>
+                    
+                    <Image src='https://images.wisegeek.com/landscape-painting.jpg' alt='Dan Abramov' height={{base: '250px', md:'400',lg: '370px' }}/>
+                   
+                </Box>
+
+                <Box
+                    height={{base: '170px', md:'500',lg: '440px' }}
+                    borderLeftColor={{base: 'white',lg: 'black' }}
+                    borderTopColor='white'
+                    borderLeftStyle='solid'
+                    borderWidth='1px'
+                    
+                    borderBottomColor={{base: 'white',lg: 'white' }}
+                    align="center"
+                    paddingTop={{base: '5px',lg:'145px' }}
+                    
+                 
+                    >
+                        
+                        <Text as='i'><b>"Beautiful Title"</b></Text><br/>
+                        <br/>
+                        <Text>Lorem ipsum dolor sit amet consectetur, adipisicin.</Text>
+                        <br/>
+                        <Text>$450</Text>
+                        <br/>
+                        <Icon as={AiTwotoneHeart}/>
+                        
+
+
                         
 
                 </Box>
             
             </SimpleGrid>
 
-        </div>
+
+
+            
+
+        </>
     )
 }
 
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,13 +1,45 @@
 import "./header.css"
-import { Text,Box } from "@chakra-ui/react";
+import { Text,Box,Center } from "@chakra-ui/react";
+import { Icon } from '@chakra-ui/react'
+import { PiWaveSineDuotone } from 'react-icons/pi';
+
 
 const Header = ()=>{
     
     return(
+        <>
+
         <Box id="header" padding='15px'>
-            <Text fontSize='4xl' as='i' id="header-text">Tamara Williams Gallery</Text>
+            <Text fontSize='4xl' as='i' id="header-text" fontFamily="sans-serif">Tamara Williams Gallery</Text>
             {/* <h1 id="header-text">Tamara Williams Gallery</h1> */}
+            
         </Box>
+        
+            <Center>
+                <Icon align="center" as={PiWaveSineDuotone}/>
+                <Icon align="center" as={PiWaveSineDuotone}/>
+                <Icon align="center" as={PiWaveSineDuotone}/>
+                <Icon align="center" as={PiWaveSineDuotone}/>
+                <Icon align="center" as={PiWaveSineDuotone}/>
+                <Icon align="center" as={PiWaveSineDuotone}/>
+                <Icon align="center" as={PiWaveSineDuotone}/>
+                <Icon align="center" as={PiWaveSineDuotone}/>
+                <Icon align="center" as={PiWaveSineDuotone}/>
+                <Icon align="center" as={PiWaveSineDuotone}/>
+                <Icon align="center" as={PiWaveSineDuotone}/>
+                <Icon align="center" as={PiWaveSineDuotone}/>
+                <Icon align="center" as={PiWaveSineDuotone}/>
+                <Icon align="center" as={PiWaveSineDuotone}/>
+                <Icon align="center" as={PiWaveSineDuotone}/>
+                <Icon align="center" as={PiWaveSineDuotone}/>
+                
+            </Center>
+            
+            
+       
+
+
+        </>
     )
 }
 
