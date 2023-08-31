67298d8ad068bfbdd12f9e0995af6d6150daadd8
antonz89 <anton.zakoverya@codeimmersives.com>
2023-08-31 11:22:44 -0500

    uploading

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,8 +1,24 @@
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
 import {Center,Box,Text,Input,Textarea,Button} from "@chakra-ui/react"
 import {Popover,PopoverTrigger,PopoverContent,PopoverHeader,PopoverCloseButton} from '@chakra-ui/react'
 
 
 const Contact = ()=>{
+
+    const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_knvm2fd', 'template_drotywi', form.current, 'ZjiIpIqNRolEKtVzm')
+      .then((result) => {
+          console.log(result.text);
+          e.target.reset();
+      }, (error) => {
+          console.log(error.text);
+      });
+  };
 
     return(
         <>
@@ -18,7 +34,7 @@ const Contact = ()=>{
                     
                 >
 
-
+                    <form ref={form} onSubmit={sendEmail}>
                         <Text 
                             align="center"
                         ><b>Contact Us</b></Text>
@@ -28,14 +44,43 @@ const Contact = ()=>{
                             placeholder='Enter Your Email' 
                             size='sm'
                             marginTop={{base:"10px",lg:"20px"}}
+                            type="email" 
+                            name="user_email" 
                             />
                         <Textarea 
                             placeholder='Enter Your Message'
                             marginTop={{base:"10px",lg:"10px"}}
                             height={{lg:"200px"}}
+                            name="message"
                             />
 
-                        <Center>
+<Center>
+
+                        <Popover>
+                            <PopoverTrigger>
+                                <Button 
+                                colorScheme='gray'
+                                marginTop="5px"
+                                type="submit" 
+                                value="Send">Send</Button>
+                            </PopoverTrigger>
+
+                            <PopoverContent>
+                                {/* <PopoverCloseButton/> */}
+                                    <PopoverHeader
+                                    bg="lightblue"
+                                    >Your Message Was Sent</PopoverHeader>
+                            </PopoverContent>
+                        </Popover>
+
+                        </Center>
+
+                    </form>
+
+                        
+                        
+                        
+                        {/* <Center>
 
                             <Popover>
                                 <PopoverTrigger>
@@ -51,7 +96,7 @@ const Contact = ()=>{
                                </PopoverContent>
                             </Popover>
 
-                        </Center>
+                        </Center> */}
 
                 </Box>
             </Center>
